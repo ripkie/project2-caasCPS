f62f768968da518f91545868e5c346df0477e75f
ripkie <rifki.dupon07@gmail.com>
2026-03-05 10:41:08 +0700

    emergency button

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,41 @@
 #include <Arduino.h>
+#include <Wire.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define BUTTON_PIN 4
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+int lastReading = LOW;
+int stableState = LOW;
+unsigned long lastDebounceMs = 0;
+const unsigned long debounceMs = 30;
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(BUTTON_PIN, INPUT);
+  Serial.println("Node A Ready (debounce)");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  int reading = digitalRead(BUTTON_PIN);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (reading != lastReading)
+  {
+    lastDebounceMs = millis();
+    lastReading = reading;
+  }
+
+  if (millis() - lastDebounceMs > debounceMs)
+  {
+    if (reading != stableState)
+    {
+      stableState = reading;
+
+      // trigger hanya saat jadi HIGH (baru ditekan)
+      if (stableState == HIGH)
+      {
+        Serial.println("EMERGENCY BUTTON PRESSED");
+      }
+    }
+  }
 }
